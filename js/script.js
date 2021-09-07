9e13d5255fe29e046b90ce6c1f71b29d67815411
Maxi Mercado <makrodevjs@gmail.com>
2021-09-06 22:21:21 -0300

    Incorporar objetos

--- js/script.js
+++ js/script.js
@@ -1,33 +1,46 @@
+class Producto {
+    constructor(nombre, categoria, precio) {
+        this.nombre  = nombre.toUpperCase();
+        this.categoria = categoria;
+        this.precio  = parseFloat(precio);
+        this.cantidad = 0;
+    }
+    
+    sumarProducto(){
+        this.cantidad++;
+    }
+
+    precioTotal(){
+        return this.precio * this.cantidad;
+    }
+}
+
 //PRODUCTOS//
-const producto1 = "Cepillo de Bambu";
-let cantProd1 = 0;
+const producto1 = new Producto("Cepillo de Bambu", "Ecologico", 650);
 
-const producto2 = "Shampoo Sólido";
-let cantProd2 = 0;
+const producto2 = new Producto("Shampoo Sólido", "Higiene", 710);
 
-const producto3 = "Acondicionador Sólido";
-let cantProd3 = 0;
+const producto3 = new Producto("Acondicionador Sólido","Higiene", 710);
 
-const producto4 = "Protectores Diarios de Tela";
-let cantProd4 = 0;
+const producto4 = new Producto("Protectores Diarios de Tela", "Sustentable", 250);
 
 //MUESTRA UN CATALOGO Y RECIBE POR EL PROMPT EL PRODUCTO ELEGIDO//
 mostrarCatalogo = () =>{
     alert("Ingrese nro de Producto que desea agregar: -->");
-    return prompt("1 - " + producto1 + "  |  2 - " + producto2 + "  |  3 - " + producto3 + "  |  4 - " + producto4);
+    return prompt("1 - " + producto1.nombre + "  |  2 - " + producto2.nombre + "  |  3 - " + producto3.nombre + "  |  4 - " + producto4.nombre);
 }
 
 //LISTA LOS PRODUCTOS QUE SU CANTIDAD SEA MAYOR A 0//
 mostrarCarrito = () =>{
     let productos = "Su carrito: "
-    if (cantProd1 > 0)
-        productos += producto1 + " - cantidad=" + cantProd1 + "  |  ";
-    if (cantProd2 > 0)
-        productos += producto2 + " - cantidad=" + cantProd2 + "  |  ";
-    if (cantProd3 > 0)
-        productos += producto3 + " - cantidad=" + cantProd3 + "  |  ";
-    if (cantProd4 > 0)
-        productos += producto4 + " - cantidad=" + cantProd4 + "  |  ";
+    if (producto1.cantidad > 0)
+        productos += producto1.nombre + " - categoria=" + producto1.categoria + " - cant=" + producto1.cantidad + " - precio= $" + producto1.precioTotal() + " |  ";
+    if (producto2.cantidad > 0)
+        productos += producto2.nombre + " - categoria=" + producto2.categoria + " - cant=" + producto2.cantidad + " - precio= $" + producto2.precioTotal() + " |  ";
+    if (producto3.cantidad > 0)
+        productos += producto3.nombre + " - categoria=" + producto3.categoria + " - cant=" + producto3.cantidad + " - precio= $" + producto3.precioTotal() + " |  ";
+    if (producto4.cantidad > 0)
+        productos += producto4.nombre + " - categoria=" + producto4.categoria + " - cant=" + producto4.cantidad + " - precio= $" + producto4.precioTotal() + " |  ";
 
     alert(productos);
 }
@@ -36,16 +49,16 @@ mostrarCarrito = () =>{
 sumarAlCarrito = nroProducto =>{
     switch (nroProducto){
         case "1":
-            cantProd1 ++;
+            producto1.sumarProducto();
             break;
         case "2":
-            cantProd2 ++;
+            producto2.sumarProducto();
             break;
         case "3":
-            cantProd3 ++;
+            producto3.sumarProducto();
             break;
         case "4":
-            cantProd4 ++;
+            producto4.sumarProducto();
             break;
     }
 }
@@ -55,7 +68,7 @@ let validacion = prompt("Bienvenido a la tienda - Ingrese 1 para ver las opcione
 
 while (validacion != "0"){
     validacion = prompt("Ingrese 1 para ver el catalogo, 2 para ver el carrito, 0 para salir");
-
+    
     if (validacion == "1"){
         let elegido = mostrarCatalogo();
         sumarAlCarrito(elegido);
